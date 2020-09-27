92ef58876b88bedad62ba389bf89fb32f5b66231
mgrzwcz <55543943+mgrzwcz@users.noreply.github.com>
2020-09-27 19:49:35 +0100

    Sunday section 6 finished and all working

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './app/reduxStore/configureStore';
+import ScrollToTop from './app/main/ScrollToTop';
 
 const store = configureStore();
 
   ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
   </Provider>
